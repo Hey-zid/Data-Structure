ca1d36c0dd0b188ebd6933741392f60c4c7c3f4f
Hey-zid <bayazidparvez3@gmail.com>
2025-12-28 04:26:50 +0600

    Division Recorrect

--- ComplexNumber.cpp
+++ ComplexNumber.cpp
@@ -4,7 +4,7 @@ using namespace std;
 //Global Structure
 struct variable
 {
-    int real;
+    float real;
     float imaginary;
 };
 
@@ -21,7 +21,7 @@ void Addition()
     variable3.real = variable1.real + variable2.real;
     variable3.imaginary =variable1.imaginary +variable2.imaginary ;
 
-    cout<<variable3.real<<" "<<variable3.imaginary;
+    cout<<variable3.real<<" + "<<variable3.imaginary<<"i";
 
 }
 
@@ -30,15 +30,15 @@ void Substruction()
     struct variable variable1,variable2,variable3;
 
     variable1.real = 5;
-    variable1.imaginary = 3.5;
+    variable1.imaginary = 6;
 
-    variable2.real = 3;
-    variable2.imaginary = 5.5;
+    variable2.real = 7;
+    variable2.imaginary = 5;
 
     variable3.real = variable1.real - variable2.real;
     variable3.imaginary =variable1.imaginary - variable2.imaginary ;
 
-    cout<<variable3.real<<" "<<variable3.imaginary;
+    cout<<variable3.real<<" + "<<variable3.imaginary<<"i";
 
 }
 
@@ -64,24 +64,41 @@ void multiplication()
 
 
 
-    cout<<variable5.real<<" "<<variable5.imaginary;
+    cout<<variable5.real<<" + "<<variable5.imaginary<<"i";
 
 }
 
 void division()
 {
-    struct variable variable1,variable2,variable3;
+    struct variable variable1,variable2,variable3,variable4,variable5,variable6,variable7,variable8,variable9;
 
     variable1.real = 5;
-    variable1.imaginary = 3.5;
+    variable1.imaginary = 6;
 
-    variable2.real = 3;
-    variable2.imaginary = 5.5;
+    variable2.real = 7;
+    variable2.imaginary = 5;
 
-    variable3.real = variable1.real / variable2.real;
-    variable3.imaginary =variable1.imaginary / variable2.imaginary ;
+    variable3.real = variable1.real * variable2.real;
+    variable3.imaginary =variable1.real * (variable2.imaginary*-1) ;
 
-    cout<<variable3.real<<" "<<variable3.imaginary;
+    variable4.real = variable1.imaginary * variable2.real;
+    variable4.imaginary =variable1.imaginary * (variable2.imaginary*-1) ;
+
+    variable5.real = variable3.real - variable4.imaginary;
+    variable5.imaginary =variable3.imaginary + variable4.real ;
+
+    variable6.real = variable2.real * variable2.real;
+    variable6.imaginary =variable2.real * (variable2.imaginary*-1) ;
+
+    variable7.real = variable2.imaginary * variable2.real;
+    variable7.imaginary =variable2.imaginary * (variable2.imaginary*-1) ;
+
+    variable8.real =variable6.real + (variable7.imaginary*-1) ;
+
+    variable9.real = variable5.real/variable8.real ;
+    variable9.imaginary = variable5.imaginary/variable8.real;
+
+    cout<<variable9.real<<" + "<<variable9.imaginary<<"i";
 
 }
 
